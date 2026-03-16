32eb5379590841e066d0172152eda08420ce6702
PVinaches <palomavinaches@gmail.com>
2026-03-16 15:12:55 +0000

    chore: clean unused export

--- packages/ui/src/pages/RestDslEditor/test-utils.ts
+++ packages/ui/src/pages/RestDslEditor/test-utils.ts
@@ -1,4 +1,4 @@
-import { fireEvent, screen, waitFor } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 
 /**
  * Opens the Actions menu and clicks a specific action item.
